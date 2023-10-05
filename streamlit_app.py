0b79aff3a6427094a9d17e8932416cb1549683df
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-10-05 10:53:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,8 +40,8 @@ def dataPivot(data, threshold, para, model):
 @st.cache_data
 def distPlot(data, para, model):
     fig = make_subplots(rows =4, cols=2)
-    fig.add_trace(go.Histogram(data, x = paraOpt+"_"+modelOpt), row =1, col =1)
-    st.plotly_chart(fig)
+    fig.add_trace(px.histogram(data, x = paraOpt+"_"+modelOpt), row =1, col =1)
+    return fig
 
 # MySQL connection and load data
 conn = st.experimental_connection("mysql", type="sql")
@@ -62,7 +62,8 @@ with col1:
 # Histogram, District, HIGHWAY_FUN, PAV_TYPE, AADT, TRUCK_PCT, tavg, prcp
         #fig = px.histogram(data_temp, paraOpt +"_"+modelOpt)
         #st.plotly_chart(fig)
-        distPlot(data= data_temp, para = paraOpt, model = modelOpt)
+        fig = distPlot(data= data_temp, para = paraOpt, model = modelOpt)
+        st.plotly_chart(fig)
 
 
 with col2:
