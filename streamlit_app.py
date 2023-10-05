73ffa1af0ce3e4fcaf45e018bc4851b0982f78ed
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-10-05 16:31:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,77 +8,6 @@ import plotly.graph_objects as go
 st.set_page_config(layout="wide")
 from urllib.request import urlopen
 import json
-from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
-from htbuilder.units import percent, px1
-from htbuilder.funcs import rgba, rgb
-
-def image(src_as_string, **style):
-    return img(src=src_as_string, style=styles(**style))
-
-def link(link, text, **style):
-    return a(_href=link, _target="_blank", style=styles(**style))(text)
-
-def layout(*args):
-    style = """
-    <style>
-      # MainMenu {visibility: hidden;}
-      footer {visibility: hidden;}
-     .stApp { bottom: 105px; }
-    </style>
-    """
-    style_div = styles(
-        position="fixed",
-        left=0,
-        bottom=0,
-        margin=px1(0, 0, 0, 0),
-        width=percent(100),
-        color="black",
-        text_align="center",
-        height="auto",
-        opacity=1
-    )
-
-    style_hr = styles(
-        display="block",
-        margin=px1(0, 0, "auto", "auto"),
-        border_style="inset",
-        border_width=px1(0.5)
-    )
-
-    body = p()
-    foot = div(
-        style=style_div
-    )(
-        hr(
-            style=style_hr
-        ),
-        body
-    )
-
-    st.markdown(style, unsafe_allow_html=True)
-
-    for arg in args:
-        if isinstance(arg, str):
-            body(arg)
-
-        elif isinstance(arg, HtmlElement):
-            body(arg)
-
-    st.markdown(str(foot), unsafe_allow_html=True)
-
-
-def footer():
-    myargs = [
-        "Made by Hongbin Xu",
-        #image('https://avatars3.githubusercontent.com/u/45109972?s=400&v=4',
-        #      width=px(25), height=px(25)),
-
-        #link("https://twitter.com/ChristianKlose3", "@ChristianKlose3"),
-        br(),
-        "The University of Texas at Austin",
-        #link("https://buymeacoffee.com/chrischross", image('https://i.imgur.com/thJhzOO.png')),
-    ]
-    layout(*myargs)
 
 
 
@@ -196,6 +125,3 @@ with col2:
         st.plotly_chart(fig,use_container_width=True)
 
     
-    
-if __name__ == "__main__":
-    footer()
