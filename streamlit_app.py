ec41e9b25afb6607e18b3254021b478299a5753b
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-10-05 00:01:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,8 +61,8 @@ with col1:
         data_temp = dataFilter(data, model = modelOpt)
 # Histogram, District, HIGHWAY_FUN, PAV_TYPE, AADT, TRUCK_PCT, tavg, prcp
         fig = px.histogram(data_temp, paraOpt +"_"+modelOpt)
-        fig.show()
-        #distPlot(data= data_temp, para = paraOpt, model = modelOpt)
+        st.plotly_chart(fig)
+                #distPlot(data= data_temp, para = paraOpt, model = modelOpt)
 
 
 with col2:
