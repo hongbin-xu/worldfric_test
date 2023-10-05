34498d02a5aa8e8d26ad6ebe8eaa4bb0758e2804
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-10-05 10:56:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ with col1:
         #fig = px.histogram(data_temp, paraOpt +"_"+modelOpt)
         #st.plotly_chart(fig)
         fig = make_subplots(rows =4, cols=2)
-        fig.add_trace(px.histogram(data_temp, x = paraOpt+"_"+modelOpt), row =1, col =1)
+        fig.add_trace(px.histogram(data_temp, paraOpt+"_"+modelOpt), row =1, col =1)
         fig = distPlot(data= data_temp, para = paraOpt, model = modelOpt)
         st.plotly_chart(fig)
 
