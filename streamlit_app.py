5f070b665bef41e9a6ed17cb059fc0a82ec08ee7
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-10-04 23:53:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 from plotly.subplots import make_subplots
-
+import plotly.graph_objects as go
 
 st.set_page_config(layout="wide")
 
@@ -19,17 +19,29 @@ def dataLoad(_conn):
     data.replace("#NAME?", np.nan, inplace = True)
     return data
 
+
+# Filter data for different model
+@st.cache_data
+def dataFilter(data, model):
+    model_data = data.loc[(data["a_"+model].notna())&(data["a_"+model].notna())].reset_index(drop = True)
+    return model_data
+
+
 # Pivot information based on the threshold
 @st.cache_data
 def dataPivot(data, threshold, para, model):
-    model_data = data.loc[(data[para+"_"+model].notna())&(data[para+"_"+model].notna())]
-    model_data["compare"] = model_data[para+"_"+model]>=threshold #compare with the threshold
-    pivot_sum = model_data.groupby(by = ['District_Number', 'District_Name', 'District_Abbr', 'County_Number', 'County_FIPS_Code', 'County_Name', "compare"]).size().reset_index(name = "count") #pivot information based on the threshold    
+    data = data.copy()
+    data["compare"] = data[para+"_"+model]>=threshold #compare with the threshold
+    pivot_sum = data.groupby(by = ['District_Number', 'District_Name', 'District_Abbr', 'County_Number', 'County_FIPS_Code', 'County_Name', "compare"]).size().reset_index(name = "count") #pivot information based on the threshold    
     return pivot_sum
+
+
 
 @st.cache_data
 def distPlot(data, para, model):
-    model_data = data.loc[(data[para+"_"+model].notna())&(data[para+"_"+model].notna())]
+    fig = make_subplots(rows =4, columns=2)
+    fig.add_trace(px.histogram(data = data, x = paraOpt+"_"+modelOpt))
+    fig.show()
 
 # MySQL connection and load data
 conn = st.experimental_connection("mysql", type="sql")
@@ -43,19 +55,19 @@ with col1:
         col11, col12 = st.columns(2)
         with col11:
             modelOpt = st.selectbox("select model:",('m1', 'm2'))
-
         with col12:
             paraOpt = st.selectbox("select parameter:", ("a", "b", "c", "t0"))
-
-
         
+        data_temp = dataFilter(data, model = modelOpt)
+# Histogram, District, HIGHWAY_FUN, PAV_TYPE, AADT, TRUCK_PCT, tavg, prcp
+        distPlot(data= data_temp, para = paraOpt, model = modelOpt)
 
 
 with col2:
     with st.container():
         st.subheader("Geo Distribution")
         varthreshold = st.slider("threshold:",  min_value=None, max_value=None, value=0)
-        pivot_info = dataPivot(data = data, threshold = varthreshold, para = paraOpt, model = modelOpt)
+        pivot_info = dataPivot(data = data_temp, threshold = varthreshold, para = paraOpt, model = modelOpt)
         st.write(pivot_info)
         # Extract transverse profile
                 
