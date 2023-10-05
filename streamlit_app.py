6c539c5520c030f385eacef1af20f36c72bf6fec
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-10-05 16:01:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,18 +9,12 @@ st.set_page_config(layout="wide")
 from urllib.request import urlopen
 import json
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 @st.cache_data
 def dataLoad(_conn):
