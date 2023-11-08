848a23ec60e74ae1f331ea7b05e1a21b10cc7781
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-11-08 12:18:08 -0600

    Update 2_Friction model.py

--- pages/2_Friction model.py
+++ pages/2_Friction model.py
@@ -129,9 +129,9 @@ if st.session_state["allow"]:
         modelOpt = st.selectbox("Select model:",('m1', 'm2'))
         with st.expander("DISTR"):
             distOpt = st.multiselect("DISTR", distr_cont["DISTR"].unique(), distr_cont["DISTR"].unique(), label_visibility="hidden")
-        list(distOpt)
+        st.write(list(distOpt))
         with st.expander("DISTR"):
-            contOpt = st.multiselect("CONT", distr_cont.loc[distr_cont["DISTR"] in modelOpt]["CONT"].values, distr_cont.loc[distr_cont["DISTR"] in modelOpt]["CONT"].values)
+            contOpt = st.multiselect("CONT", distr_cont.loc[distr_cont["DISTR"] in modelOpt]["CONT"].values, distr_cont.loc[distr_cont["DISTR"] in distOpt]["CONT"].values)
         highOpt = st.multiselect("Facility", ("FM", "SH", "US", "IH"),("FM", "SH", "US", "IH"))
         pavOpt = st.multiselect("Pavement", ("AC_Thin", "AC_Thick", "COM", "JCP", "CRCP"), ("AC_Thin", "AC_Thick", "COM", "JCP", "CRCP"))
 
