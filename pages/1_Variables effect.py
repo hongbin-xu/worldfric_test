ffee8f40160d7dfdda299899161e5a27f7f47722
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-11-08 10:41:14 -0600

    Update 1_Variables effect.py

--- pages/1_Variables effect.py
+++ pages/1_Variables effect.py
@@ -17,6 +17,41 @@ st.set_page_config(layout="wide",
 
 st.sidebar.success("Main")
 
+# Authentication function
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if (
+            st.session_state["username"] in st.secrets["passwords"]
+            and st.session_state["password"]
+            == st.secrets["passwords"][st.session_state["username"]]
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store username + password
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show inputs for username + password.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 User not known or password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
 
 @st.cache_data
 def dataLoad(_conn):
@@ -133,5 +168,7 @@ if st.session_state["allow"]:
                             hover_data = ["District_Name", "County_Name", "count"])
             fig.update_geos(fitbounds="locations")
             st.plotly_chart(fig,use_container_width=True)
+else:
+    st.session_state["allow"] = check_password()
 
         
