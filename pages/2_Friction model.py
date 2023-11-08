4072c83f709d621a1aa196bba4cf040795abbb0a
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-11-08 10:41:19 -0600

    Update 2_Friction model.py

--- pages/2_Friction model.py
+++ pages/2_Friction model.py
@@ -1,12 +1,54 @@
-
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+from urllib.request import urlopen
+import json
 
 st.set_page_config(layout="wide", 
-                   page_title='Friction Modeling', 
+                   page_title='Friction model', 
                    menu_items={
                        'Get help': "mailto:hongbinxu@utexas.edu",
                        'About': "Developed and maintained by Hongbin Xu",
                    })
 
+# Authentication function
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if (
+            st.session_state["username"] in st.secrets["passwords"]
+            and st.session_state["password"]
+            == st.secrets["passwords"][st.session_state["username"]]
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store username + password
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show inputs for username + password.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 User not known or password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
 
 
 @st.cache_data
