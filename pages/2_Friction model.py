b8c39cd88bc5e69fed87123074cebdee09bba124
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-11-08 12:15:26 -0600

    Update 2_Friction model.py

--- pages/2_Friction model.py
+++ pages/2_Friction model.py
@@ -128,7 +128,9 @@ if st.session_state["allow"]:
     with st.sidebar:
         modelOpt = st.selectbox("Select model:",('m1', 'm2'))
         with st.expander("DISTR"):
-            distOpt = st.multiselect("DISTR", distr_cont["DISTR"].unique(), distr_cont["DISTR"].unique(), key = False, label_visibility="hidden")
+            distOpt = st.multiselect("DISTR", distr_cont["DISTR"].unique(), distr_cont["DISTR"].unique(), label_visibility="hidden")
+
+        distOpt
             
         with st.expander("DISTR"):
             contOpt = st.multiselect("CONT", distr_cont.loc[distr_cont["DISTR"] in modelOpt]["CONT"].values, distr_cont.loc[distr_cont["DISTR"] in modelOpt]["CONT"].values)
