677460da06a5e042035a08f9dd687bba9a91f99b
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-11-16 10:14:39 -0600

    Update 2_Friction model.py

--- pages/2_Friction model.py
+++ pages/2_Friction model.py
@@ -61,7 +61,7 @@ def dataLoad(_conn):
     mode2: select for multiple segment
     creating 2d array of the height measurement
     """
-    data = conn.query('SELECT * from fricPAwh_v1;') 
+    data = conn.query('SELECT * from fricPAwh;') 
     distr_cont = conn.query('SELECT * from distr_cont_onlineApp;') 
     return data, distr_cont
 
